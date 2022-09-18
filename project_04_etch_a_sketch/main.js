efda081ba32572463b551d6d86d84fcf7a77f46e
Michael Gombos <71684424+MichaelGombos@users.noreply.github.com>
2022-09-18 15:58:12 -0500

    add grid, add ability to draw on boxes and reset

--- project_04_etch_a_sketch/main.js
+++ project_04_etch_a_sketch/main.js
@@ -1,1 +1,45 @@
-console.log("etch a sketch");
+const grid = document.getElementById("grid");
+const widthElement = document.getElementById("width");
+const heightElement = document.getElementById("height");
+const delayElement = document.getElementById("delay");
+const reset = document.getElementById("reset");
+
+let width = 16;
+let height = 16;
+
+const activate = (e) =>{
+  e.target.classList.add("active");
+}
+
+const deActivate = (e) =>{
+  setTimeout(()=>{
+    e.target.classList.remove("active");
+  },5000);
+  
+}
+
+
+//create rows
+for(let i =0; i < width; i++){
+  let newRow = document.createElement("div");
+  newRow.classList.add("grid-row");
+  grid.appendChild(newRow);
+    //create grid item
+    for(let j = 0; j < height; j++){
+      let newGridBox = document.createElement("div");
+      newGridBox.classList.add("grid-box");
+      newGridBox.addEventListener("mouseenter",activate);
+      newGridBox.addEventListener("mouseleave",deActivate);
+
+      newRow.appendChild(newGridBox);
+    }
+}
+
+//reset button
+const resetHandler = () => {
+  for(item of Array.from(document.getElementsByClassName("grid-box"))){
+    item.classList.remove("active");
+  }
+}
+
+reset.addEventListener("click",resetHandler);
