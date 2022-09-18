4e413810cbfc7b9c2f8f28bea8887cc345270daa
Michael Gombos <71684424+MichaelGombos@users.noreply.github.com>
2022-09-18 16:23:44 -0500

    add ability to modify delay, and size of grid.

--- project_04_etch_a_sketch/main.js
+++ project_04_etch_a_sketch/main.js
@@ -3,9 +3,15 @@ const widthElement = document.getElementById("width");
 const heightElement = document.getElementById("height");
 const delayElement = document.getElementById("delay");
 const reset = document.getElementById("reset");
+const set = document.getElementById("set");
 
-let width = 16;
-let height = 16;
+widthElement.defaultValue = 16;
+heightElement.defaultValue = 16;
+delayElement.defaultValue = 5000;
+
+let width = widthElement.value;
+let height = heightElement.value;
+let delay = delayElement.defaultValue;
 
 const activate = (e) =>{
   e.target.classList.add("active");
@@ -20,20 +26,29 @@ const deActivate = (e) =>{
 
 
 //create rows
-for(let i =0; i < width; i++){
-  let newRow = document.createElement("div");
-  newRow.classList.add("grid-row");
-  grid.appendChild(newRow);
-    //create grid item
-    for(let j = 0; j < height; j++){
-      let newGridBox = document.createElement("div");
-      newGridBox.classList.add("grid-box");
-      newGridBox.addEventListener("mouseenter",activate);
-      newGridBox.addEventListener("mouseleave",deActivate);
-
-      newRow.appendChild(newGridBox);
-    }
+const generateGrid = () =>{
+  for(let i =0; i < height; i++){
+    let newRow = document.createElement("div");
+    newRow.classList.add("grid-row");
+    grid.appendChild(newRow);
+      //create grid item
+      for(let j = 0; j < width; j++){
+        let newGridBox = document.createElement("div");
+        newGridBox.classList.add("grid-box");
+        newGridBox.addEventListener("mouseenter",activate);
+        newGridBox.addEventListener("mouseleave",deActivate);
+  
+        newRow.appendChild(newGridBox);
+      }
+  }
 }
+const deleteGrid = () =>{
+ 
+  for(item of Array.from(document.getElementsByClassName("grid-row"))){
+    item.remove();
+  }
+}
+generateGrid();
 
 //reset button
 const resetHandler = () => {
@@ -42,4 +57,18 @@ const resetHandler = () => {
   }
 }
 
+const setHandler = (e) => {
+
+  e.preventDefault();
+  //takes width height and delay
+  width = widthElement.value;
+  height = heightElement.value;
+  delay = delayElement.value;
+  //redraw board 
+  resetHandler();
+  deleteGrid();
+  generateGrid();
+}
 reset.addEventListener("click",resetHandler);
+
+set.addEventListener("click",setHandler);
