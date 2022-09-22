3ab5585caa2692ed444a14aba1084147448de6bd
Michael Gombos <71684424+MichaelGombos@users.noreply.github.com>
2022-09-22 08:28:01 -0500

    add operate function

--- project_05_calculator/main.js
+++ project_05_calculator/main.js
@@ -1,5 +1,25 @@
-console.log("calculator");
-
 const operate = (operator,num1,num2) =>{
-
+  let result;
+  switch(operator){
+    case "add":
+      result = num1+num2;
+      break;
+    case "subtract":
+      result = num1-num2;
+      break;
+    case "multiply":
+      result = num1*num2;
+      break;
+    case "divide":
+      //check for div by 0 error
+      result = num1/num2;
+      break;
+    case "percentage":
+      result = (num1/100) * num2;
+      break;    
+    default:
+      result = "INVALID OPERATOR";
+      break;
+  }
+  return result;
 }
