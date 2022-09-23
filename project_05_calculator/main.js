84d96677389e3e72156a0508f4ed6534d594a446
Michael Gombos <71684424+MichaelGombos@users.noreply.github.com>
2022-09-23 08:22:09 -0500

    initialize dom elements, add empty handlers for buttons

--- project_05_calculator/main.js
+++ project_05_calculator/main.js
@@ -23,3 +23,50 @@ const operate = (operator,num1,num2) =>{
   }
   return result;
 }
+
+const operatorHandler = (command) => (e) =>{
+  console.log(command);
+}
+
+const commandHandler = (command) => (e) =>{
+  console.log(command);
+}
+
+const numberHandler = (command) => (e) =>{
+  console.log(command);
+}
+
+let inputObject = {
+  result : 0,
+  nextNumber : 0,
+  operator :0 ,
+  steps: []
+}
+
+//initiating elements
+const operators = ["percentage","divide","multiply", "subtract","add"]
+
+const commands = ["clear","sign","equals","decimal"];
+
+const numbers = [1,2,3,4,5,6,7,8,9,0];
+
+const displayBox = document.getElementById("display");
+const display = displayBox.childNodes[0];
+
+const allButtons = Array.from(document.getElementsByClassName("button"));
+
+const operatorButtons = allButtons.filter(button => operators.includes(button.dataset.command));
+
+const commandButtons = allButtons.filter(button => commands.includes(button.dataset.command));
+
+const numberButtons =  allButtons.filter(button => numbers.includes(parseInt(button.dataset.command)));
+
+
+operatorButtons.forEach(button => button.addEventListener("click",numberHandler(button.dataset.command)));
+
+commandButtons.forEach(button => button.addEventListener("click",numberHandler(button.dataset.command)));
+
+numberButtons.forEach(button => button.addEventListener("click",numberHandler(button.dataset.command)));
+
+
+
