dc8f886ffba72ad034cb25a89ff7ddc8ab11e2ee
Michael Gombos <71684424+MichaelGombos@users.noreply.github.com>
2022-09-25 19:06:12 -0500

    add handler for form submit

--- project_06_sign-up_form/main.js
+++ project_06_sign-up_form/main.js
@@ -1,1 +1,29 @@
 console.log("sign up form");
+
+let formObject = {
+  first:"",
+  last:"",
+  email:"",
+  phone:"",
+  password:"",
+  confirm_password:""
+}
+
+const handleFormSubmit = (e) => {
+  formObject.first = document.getElementById("first").value;
+  formObject.last = document.getElementById("last").value;
+  formObject.email = document.getElementById("email").value;
+  formObject.phone = document.getElementById("phone").value;
+  formObject.password = document.getElementById("password").value;
+  formObject.confirm_password = document.getElementById("confirm-password").value;
+
+  alert("information entered :: " + JSON.stringify(formObject));
+
+  e.preventDefault();
+}
+
+const form = document.getElementsByTagName("form");
+
+const submitButton = document.getElementById("form-submit");
+
+submitButton.addEventListener("click",handleFormSubmit);
