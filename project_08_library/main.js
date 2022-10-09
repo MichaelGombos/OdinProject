3e48709ebd18148d84acecf4483902f8e5196473
Michael Gombos <71684424+MichaelGombos@users.noreply.github.com>
2022-10-08 19:34:40 -0500

    add library and functions to interact with book

--- project_08_library/main.js
+++ project_08_library/main.js
@@ -1,5 +1,7 @@
 console.log('Library ');
 
+const library = [];
+
 function Book(title, author, pages, read){
   this.title = title;
   this.author = author;
@@ -11,6 +13,27 @@ Book.prototype.info = function() {
   console.log("BOOK INFO::");
   console.log(this.title,this.author,this.pages,this.read);
 }
- 
+
+function addBookToLibrary(book) {
+  library.push(book);
+}
+
+function removeFromLibrary(bookToRemove) {
+  let  index = library.findIndex(book => book.title === bookToRemove.title);
+  library.splice(index,1);
+  console.log("removing 1 item at index :: " +index );
+}
+
+function toggleRead(book) {
+  book.read = !book.read;
+}
+
 let Harry = new Book("harry","jk","400",true);
+let Barry = new Book("barry","bk","600",false);
 Harry.info();
+
+addBookToLibrary(Harry);
+addBookToLibrary(Barry);
+removeFromLibrary(Harry);
+
+console.log(library);
