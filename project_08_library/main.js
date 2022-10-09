f341433b711c7982c5e05814f2d7b8e267c00fb1
Michael Gombos <71684424+MichaelGombos@users.noreply.github.com>
2022-10-09 15:30:22 -0500

    add event listeners, able to upload books using the form

--- project_08_library/main.js
+++ project_08_library/main.js
@@ -1,7 +1,3 @@
-console.log('Library ');
-
-const library = [];
-
 function Book(title, author, pages, read){
   this.title = title;
   this.author = author;
@@ -9,31 +5,79 @@ function Book(title, author, pages, read){
   this.read = read;
 }
 
-Book.prototype.info = function() {
-  console.log("BOOK INFO::");
-  console.log(this.title,this.author,this.pages,this.read);
+const libraryTableBody = document.getElementsByTagName("tbody")[0];
+const formSubmit = document.getElementById("form-submit");
+const formTitle = document.getElementById("title");
+const formAuthor = document.getElementById("author");
+const formPages = document.getElementById("pages");
+const formRead = document.getElementById("read-yes");
+
+function appendBook(book){
+  const newBook = document.createElement("tr");
+  newBook.classList.add("tag-row");
+
+  const newBookTitle = document.createElement("td");
+  newBookTitle.appendChild(document.createTextNode(book.title));
+
+  const newBookAuthor = document.createElement("td");
+  newBookAuthor.appendChild(document.createTextNode(book.author));
+
+  const newBookPages = document.createElement("td");
+  newBookPages.appendChild(document.createTextNode(book.pages));
+
+  const newBookRead = document.createElement("td");
+  const newBookReadInput = document.createElement("input");
+  newBookReadInput.type = "checkbox";
+  newBookReadInput.name = "read-toggle";
+  newBookReadInput.checked = book.read;
+
+  const newBookReadLabel = document.createElement("label");
+  newBookReadLabel.appendChild(document.createTextNode(book.read ? "Yes" : "No"));
+  newBookRead.addEventListener("click",handleToggleRead);
+  
+  newBookRead.appendChild(newBookReadInput);
+  newBookRead.appendChild(newBookReadLabel);
+
+  const newBookDelete = document.createElement("td");
+  const newBookDeleteButton = document.createElement("button");
+  newBookDeleteButton.appendChild(document.createTextNode("Delete"));  
+  newBookDeleteButton.classList.add("button")
+  newBookDeleteButton.addEventListener("click",handleDelete);
+  newBookDelete.appendChild(newBookDeleteButton);
+
+  newBook.appendChild(newBookTitle);
+  newBook.appendChild(newBookAuthor);
+  newBook.appendChild(newBookPages);
+  newBook.appendChild(newBookRead);
+  newBook.appendChild(newBookDelete);
+
+  libraryTableBody.appendChild(newBook);
+  console.log(newBook);
 }
 
-function addBookToLibrary(book) {
-  library.push(book);
+const handleFormSubmit = (e) =>{
+  const formBook = new Book(formTitle.value,formAuthor.value,formPages.value,formRead.checked);
+  appendBook(formBook);
+  e.preventDefault();
 }
 
-function removeFromLibrary(bookToRemove) {
-  let  index = library.findIndex(book => book.title === bookToRemove.title);
-  library.splice(index,1);
-  console.log("removing 1 item at index :: " +index );
+const handleDelete = (e) =>{
+  e.target.parentElement.parentElement.remove()
 }
 
-function toggleRead(book) {
-  book.read = !book.read;
+const handleToggleRead = (e) =>{
+  e.currentTarget.children[0].checked ? e.currentTarget.children[1].innerHTML = "Yes" : e.currentTarget.children[1].innerHTML = "No";
 }
 
-let Harry = new Book("harry","jk","400",true);
-let Barry = new Book("barry","bk","600",false);
-Harry.info();
 
-addBookToLibrary(Harry);
-addBookToLibrary(Barry);
-removeFromLibrary(Harry);
+let CODE = new Book("CODE : The Hidden Language Of Computer Hardware and Software","Charles Petzold","382",false);
 
-console.log(library);
+let TheUXTeamOfOne = new Book("The User Experience Team Of One","Leah Buley","235",true);
+
+let ThePragmaticProgrammer = new Book("The Pragmatic Programmer","David Thomas","306",false);
+
+appendBook(CODE);
+appendBook(TheUXTeamOfOne);
+appendBook(ThePragmaticProgrammer);
+
+formSubmit.addEventListener("click",handleFormSubmit);
